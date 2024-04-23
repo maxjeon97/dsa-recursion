b591854901c7fb289b150734b16e8910728ad7e4
Thomas Kim <tkimm156@gmail.com>
2024-04-23 13:55:34 -0500

    find and longest

--- recursion/find/find.ts
+++ recursion/find/find.ts
@@ -1,7 +1,15 @@
 /** find: return t/f is val is in arr. */
 
 function find(arr: number[], val: number): boolean {
-  return true;
+  if (arr.length === 0) {
+    return false;
+  }
+  if (arr[0] === val) {
+    return true;
+  } else {
+    return find(arr.slice(1), val);
+  }
+
 }
 
 export { find };
--- recursion/longest/longest.ts
+++ recursion/longest/longest.ts
@@ -3,7 +3,13 @@ import { LLNodeStr } from "../common/ll";
 /** longest: return length of longest word in a linked list of words. */
 
 function longest(words: LLNodeStr | null): number {
-  return 42;
+  if (words === null) {
+    return 0;
+  }
+
+  return Math.max(words.val.length, longest(words.next));
 }
+
+
 
 export { longest };
