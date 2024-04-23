56cbf90e765befedcefef0e93ace2965e4e35e24
Max Jeon <maxjeon97@gmail.com>
2024-04-23 12:00:05 -0700

    Did isPalindrome

--- recursion/is-palindrome/isPalindrome.ts
+++ recursion/is-palindrome/isPalindrome.ts
@@ -1,7 +1,16 @@
 /** isPalindrome: checks whether a string is a palindrome or not. */
 
 function isPalindrome(str: string): boolean {
-  return true;
+  if (str.length === 0) {
+    return true;
+  }
+
+  if (str[0] === str[str.length - 1]) {
+    return isPalindrome(str.slice(1, str.length - 1));
+  }
+  else {
+    return false;
+  }
 }
 
 export { isPalindrome };
