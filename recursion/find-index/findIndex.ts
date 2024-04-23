1b53b04341efc25ec8adf2bbb03fdfe7f0810122
Thomas Kim <tkimm156@gmail.com>
2024-04-23 14:18:56 -0500

    continue to solve problems

--- recursion/find-index/findIndex.ts
+++ recursion/find-index/findIndex.ts
@@ -1,7 +1,16 @@
 /** findIndex: return index of val in arr (or -1 if val is not present). */
 
 function findIndex(arr: number[], val: number): number {
-  return 42;
+  if (arr.length === 0) return -1;
+
+
+  if (arr[0] === val) {
+    return 0;
+  }
+
+  const output = findIndex(arr.slice(1), val);
+
+  return output === -1 ? -1 : output + 1;
 }
 
 export { findIndex };
--- recursion/rev-string/revString.ts
+++ recursion/rev-string/revString.ts
@@ -1,7 +1,12 @@
 /** revString: return a copy of a string, but in reverse. */
 
 function revString(str: string): string {
-  return "x";
+  if (str.length === 0) {
+    return "";
+  }
+
+  // return str[str.length - 1] + revString(str.slice(0, str.length - 1));
+  return revString(str.slice(1)) + str[0];
 }
 
 export { revString };
