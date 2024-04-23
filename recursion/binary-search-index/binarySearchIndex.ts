2c98b232fb4fe87677485aa9897f8c81143520e7
Thomas Kim <tkimm156@gmail.com>
2024-04-23 16:11:36 -0500

    lmao pojos

--- recursion/binary-search-index/binarySearchIndex.ts
+++ recursion/binary-search-index/binarySearchIndex.ts
@@ -2,11 +2,24 @@
  * return the index of that value (or -1 if val is not present). */
 
 function binarySearchIndex(
-    arr: number[],
-    val: number,
-    left = 0,
-    right = arr.length): number {
-  return 42;
+  arr: number[],
+  val: number,
+  left = 0,
+  right = arr.length): number {
+  if (left > right) {
+    return -1;
+  }
+
+  const middle = Math.floor((right + left) / 2);
+  if (arr[middle] === val) {
+    return middle;
+  }
+  else if (arr[middle] > val) {
+    return binarySearchIndex(arr, val, left, middle - 1);
+  }
+  else {
+    return binarySearchIndex(arr, val, middle + 1, right);
+  }
 }
 
 export { binarySearchIndex };
--- recursion/gather-strings/gatherStrings.ts
+++ recursion/gather-strings/gatherStrings.ts
@@ -1,7 +1,17 @@
 /** gatherStrings: given an object, return array of all the string values. */
 
 function gatherStrings(obj: Record<string, any>): string[] {
-  return ["x"];
+  let strings: string[] = [];
+
+  for (const key in obj) {
+    if (typeof obj[key] === "string") {
+      strings.push(obj[key]);
+    } else if (typeof obj[key] === "object") {
+      strings.push(...gatherStrings(obj[key]));
+
+    }
+  }
+  return strings;
 }
 
 export { gatherStrings };
