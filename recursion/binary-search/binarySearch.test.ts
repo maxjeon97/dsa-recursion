794ddc57ae364bb61e85740f6bef07aced945e49
Max Jeon <maxjeon97@gmail.com>
2024-04-23 13:44:21 -0700

    Complete binarySearch

--- recursion/binary-search/binarySearch.test.ts
+++ recursion/binary-search/binarySearch.test.ts
@@ -1,7 +1,7 @@
 import { it, expect } from "vitest";
 import { binarySearch } from "./binarySearch";
 
-it("should find the index of a value in a sorted array", function () {
+it("should return true if the value is found in a sorted array", function () {
   expect(binarySearch([1, 2, 3, 4], 4)).toEqual(true);
   expect(binarySearch([1, 2], 1)).toEqual(true);
   expect(binarySearch([1, 2, 3, 4, 5, 6, 7], 6)).toEqual(true);
@@ -11,4 +11,5 @@ it("should return false if the value is not found", function () {
   expect(binarySearch([], 0)).toEqual(false);
   expect(binarySearch([1, 2, 3, 4], 0)).toEqual(false);
   expect(binarySearch([1, 2], 11)).toEqual(false);
+  expect(binarySearch([1, 2, 3, 4, 5, 6], 7)).toEqual(false);
 });
--- recursion/binary-search/binarySearch.ts
+++ recursion/binary-search/binarySearch.ts
@@ -1,8 +1,27 @@
 /** binarySearch: given a sorted array of numbers, and a value,
  * return true if val is in array, false if not present). */
 
-function binarySearch(arr: number[], val: number, left = 0, right = arr.length): boolean {
-  return false;
+function binarySearch(arr: number[], val: number, left = 0, right = arr.length - 1): boolean {
+  if (left > right) {
+    return false;
+  }
+
+  const middle = Math.floor((right + left) / 2);
+
+  if (arr[middle] === val) {
+    return true;
+  }
+  else if (arr[middle] > val) {
+    return binarySearch(arr, val, left, middle - 1);
+  }
+  else {
+    return binarySearch(arr, val, middle + 1, right);
+  }
 }
+
+// const arr = [1, 2, 3, 4, 5, 6] val = 7
+/**
+ * is 0 >= 6? no => middle = 3 => arr[3] < 8 => binarySearch(arr, val=8, left=5, right=6)
+ */
 
 export { binarySearch };
